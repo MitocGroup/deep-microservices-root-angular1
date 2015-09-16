a747097dce64e0ee92d37a9c1858b18da413b937
vcernomschi <vcernomschi@vcernomschis-MacBook-Pro.local>
2015-09-16 18:15:33 +0300

    Fix shell script

--- test/bin/_head.sh
+++ test/bin/_head.sh
@@ -1,19 +1,30 @@
 #!/usr/bin/env bash
 
 __SCRIPT_PATH=$(cd $(dirname $0); pwd -P)
-__RES_PATH="${__SCRIPT_PATH}/../resources"
 
 __ROOT_PATH="${__SCRIPT_PATH}/../../"
-__SRC_PATH="${__ROOT_PATH}src/"
+__SRC_PATH="${__ROOT_PATH}src/Tests/"
 
 subpath_run_cmd () {
     local DIR
     local CMD
+    local EXPR
 
     DIR=$(cd $1 && pwd -P)
     CMD=$2
 
-    cd $DIR && eval_or_exit "$CMD"
+    if [ -z $3 ]; then
+        EXPR="*"
+    else
+        EXPR=$3
+    fi
+
+    for subpath in $DIR/$EXPR
+    do
+        if [ -d ${subpath} ]; then
+            cd ${subpath} && eval_or_exit "$CMD"
+        fi
+    done
 }
 
 eval_or_exit() {
