f6f202b407fbf72825d463703ce19c5e13dc5f49
vcernomschi <vcernomschi@vcernomschis-MacBook-Pro.local>
2015-09-16 23:07:38 +0300

    Coverage loging

--- test/bin/coverage.sh
+++ test/bin/coverage.sh
@@ -13,19 +13,21 @@ echo "__SRC_PATH:"
 cd ${__SRC_PATH}
 ls -l
 
+echo "starting combining:"
 ### Merge Coverage results ###
 istanbul-combine -d ${__COVERAGE_PATH} -r lcov -p both \
   ${__SRC_PATH}Frontend/coverage/*/coverage-final.json
 
 ### Upload Coverage info to Codacy ###
+echo "Done combining"
 cd ${__COVERAGE_PATH}
 ls -l
 
 cat ${__COVERAGE_PATH}"/lcov.info" | codacy-coverage
 cat ${__COVERAGE_PATH}"/lcov.info" | coveralls
-#
-#### Cleanup! ###
-#
-#__CMD='rm -rf ./coverage'
-#
-#subpath_run_cmd ${__SRC_PATH} "$__CMD"
+
+### Cleanup! ###
+
+__CMD='rm -rf ./coverage'
+
+subpath_run_cmd ${__SRC_PATH} "$__CMD"
