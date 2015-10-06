709d2f4308576310981be18ad0ed1d23ad913fe0
AlexanderC <alexander.moldova@gmail.com>
2015-10-05 18:32:37 -0700

    implement framework automated pull

--- src/hook.server.js
+++ src/hook.server.js
@@ -2,40 +2,41 @@
  * Created by AlexanderC on 10/6/15.
  */
 
-var DEEP = 'deep-framework';
-var BROWSER_BUILD = 'browser/framework.js';
-var FW_DEP_PATH = 'DeepNgRoot/Frontend/js/lib/deep-framework.js';
+var exports = module.exports = function() {
+  var DEEP = 'deep-framework';
+  var BROWSER_BUILD = 'browser/framework.js';
+  var FW_DEP_PATH = 'DeepNgRoot/Frontend/js/lib/deep-framework.js';
 
-var exec = require("child_process").exec;
-var path = require('path');
-var fs = require('fs');
+  var exec = require("child_process").exec;
+  var path = require('path');
+  var fs = require('fs');
 
-console.log('- Checking for ' + DEEP + ' globally');
+  console.log('- Checking for ' + DEEP + ' globally');
 
-var installation = exec(
-  'npm info -g ' + DEEP + ' --loglevel silent > /dev/null || npm install -g ' + DEEP + ' --production --loglevel warn',
-  function(error, stdout, stderr) {
-    if (error) {
-      console.error('Error while installing ' + DEEP, error);
-      process.exit(1);
-    }
-
-    exec('npm root -g', function(error, stdout, stderr) {
+  var installation = exec(
+    'npm info -g ' + DEEP + ' --loglevel silent > /dev/null || npm install -g ' + DEEP + ' --production --loglevel warn',
+    function(error, stdout, stderr) {
       if (error) {
-        console.error('Error getting NPM root', error);
+        console.error('Error while installing ' + DEEP, error);
         process.exit(1);
       }
 
-      var npmRoot = stdout.replace(/\s+/, '');
-      var fw = path.join(npmRoot, DEEP);
-      var browserFw = path.join(fw, BROWSER_BUILD);
+      exec('npm root -g', function(error, stdout, stderr) {
+        if (error) {
+          console.error('Error getting NPM root', error);
+          process.exit(1);
+        }
 
-      if (!fs.existsSync(browserFw)) {
-        console.error('Missing browser version of ' + DEEP + ' in ' + browserFw);
-        process.exit(1);
-      }
+        var npmRoot = stdout.replace(/\s+/, '');
+        var fw = path.join(npmRoot, DEEP);
+        var browserFw = path.join(fw, BROWSER_BUILD);
 
-      var fwDep = path.join(__dirname, FW_DEP_PATH);
+        if (!fs.existsSync(browserFw)) {
+          console.error('Missing browser version of ' + DEEP + ' in ' + browserFw);
+          process.exit(1);
+        }
+
+        var fwDep = path.join(__dirname, FW_DEP_PATH);
 
         exec('rm -f ' + fwDep + '; cp ' + browserFw + ' ' + fwDep, function(error, stdout, stderr) {
           if (error) {
@@ -45,9 +46,10 @@ var installation = exec(
 
           console.log('Browser version of ' + DEEP + ' was successfully copied into ' + fwDep);
         });
-    });
-  }
-);
+      });
+    }
+  );
 
-installation.stdout.pipe(process.stdout);
-installation.stderr.pipe(process.stderr);
+  installation.stdout.pipe(process.stdout);
+  installation.stderr.pipe(process.stderr);
+};
