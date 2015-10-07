306435e5d268e31dc0f832a74f58f5a1d5e4f9ae
vcernomschi <vcernomschi@vcernomschis-MacBook-Pro.local>
2015-10-07 14:00:52 +0300

    Fix jscs

--- src/DeepNgRoot/Tests/Frontend/angular/directives/updateTitle.spec.js
+++ src/DeepNgRoot/Tests/Frontend/angular/directives/updateTitle.spec.js
@@ -12,7 +12,7 @@ describe('Directives', function() {
 
   // Store references to $rootScope and $compile
   // so they are available to all tests in this describe block
-  beforeEach(inject(function(_$compile_, _$rootScope_){
+  beforeEach(inject(function(_$compile_, _$rootScope_) {
     // The injector unwraps the underscores (_) from around the parameter names when matching
     $compile = _$compile_;
     $rootScope = _$rootScope_;
@@ -21,8 +21,10 @@ describe('Directives', function() {
   it('Check that title doesn\'t equal \'It works!\'', function() {
     // Compile a piece of HTML containing the directive
     var element = $compile('<title update-title></title>')($rootScope);
+
     // fire all the watches, to see that title doesn't equal to 'It works!'
     $rootScope.$digest();
+
     // Check that the compiled element contains the templated content
     expect(element.html()).not.toContain('It works!');
   });
