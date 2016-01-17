f372ef9b9d1110ca621b2b1059bda121f1e7397d
vcernomschi <vcernomschi@mitocgroup.com>
2016-01-17 13:36:20 +0200

    Fix formatting

--- src/DeepNgRoot/hook.init.js
+++ src/DeepNgRoot/hook.init.js
@@ -4,7 +4,7 @@
 
 'use strict';
 
-var exports = module.exports = function (callback) {
+module.exports = function(callback) {
   var exec = require('child_process').exec;
   var path = require('path');
   var fs = require('fs');
@@ -25,7 +25,7 @@ var exports = module.exports = function (callback) {
     // || npm install -g ' + DEEP + ' --production --loglevel warn &>/dev/null',
     'npm list -g --depth 1 ' + DEEP_TEMP +
     ' || npm install -g ' + DEEP_TEMP + ' --production --loglevel warn',
-    function (error) {
+    function(error) {
       if (error) {
         console.error('Error while installing ' + DEEP, error);
         callback();
@@ -34,7 +34,7 @@ var exports = module.exports = function (callback) {
 
       console.log('No errors, will run "npm root -g"');
 
-      exec('npm root -g', function (error, stdout) {
+      exec('npm root -g', function(error, stdout) {
         if (error) {
           console.error('Error getting NPM root', error);
           callback();
@@ -61,7 +61,7 @@ var exports = module.exports = function (callback) {
         var fwDep = path.join(__dirname, FW_DEP_PATH);
         console.log('fwDep: ', fwDep);
         if (!fs.existsSync(fwDep)) {
-          exec('cp ' + browserFw + ' ' + fwDep, function (error) {
+          exec('cp ' + browserFw + ' ' + fwDep, function(error) {
             if (error) {
               console.error('Error while copying browser version of ' + DEEP, error);
               callback();
