995069b84ee1fe8cf9cf248f6b9ca3cc3d8530bc
dragosh1011 <dragos.palade94@gmail.com>
2016-05-04 13:25:40 +0300

    Disable angular debugging for production and stage environment #52

--- src/DeepRootAngular/Frontend/js/app/module/ng-config.js
+++ src/DeepRootAngular/Frontend/js/app/module/ng-config.js
@@ -5,14 +5,20 @@ export class Config {
   /**
    *
    * @param {Boolean} isLocalhost
+   * @param {String} env
    * @param {String} ngRewrite
    * @param {Object} $locationProvider
+   * @param {Object} $compileProvider
    */
-  constructor(isLocalhost, ngRewrite, $locationProvider) {
+  constructor(isLocalhost, env, ngRewrite, $locationProvider, $compileProvider) {
     let isS3Website = /\.amazonaws\.com$/i.test(window.location.hostname);
 
     if (!isS3Website && !isLocalhost && ngRewrite === '/') {
       $locationProvider.html5Mode(true);
+    }
+
+    if (env === 'prod' || env === 'stage') {
+      $compileProvider.debugInfoEnabled(false);
     }
   }
 }
