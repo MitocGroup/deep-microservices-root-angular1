8cb0722eec5a4c08da7da3c41aee7761cbf137da
vcernomschi <vcernomschi@mitocgroup.com>
2016-06-07 01:08:57 +0300

    Regenerate backend tests

--- src/deep-root-angular/tests/backend/node-bin/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-npm link chai &&\
-npm link aws-sdk &&\
-npm link node-dir &&\
-npm link deepify &&\
-ln -s ../../../backend/src/async-config ./node_modules &&\
-ln -s ../../../backend/src/scheduler ./node_modules
