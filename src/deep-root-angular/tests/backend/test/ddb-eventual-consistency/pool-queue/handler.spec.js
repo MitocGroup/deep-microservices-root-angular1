b248042435ebc0262ef0a539601c17a12c9a22a9
vcernomschi <vcernomschi@mitocgroup.com>
2016-06-13 17:02:05 +0300

    Add backend tests

---
+++ src/deep-root-angular/tests/backend/test/ddb-eventual-consistency/pool-queue/handler.spec.js
@@ -0,0 +1,13 @@
+// THIS TEST WAS GENERATED AUTOMATICALLY ON Mon Jun 13 2016 16:56:19 GMT+0300 (EEST)
+
+'use strict';
+
+import chai from 'chai';
+import Handler from '../../../node_modules/ddb-eventual-consistency/pool-queue/Handler';
+
+// @todo: Add more advanced tests
+suite('Handlers', () => {
+  test('Class Handler exists in deep-system-ddb-eventual-consistency-pool-queue module', () => {
+    chai.expect(Handler).to.be.an('function');
+  });
+});
