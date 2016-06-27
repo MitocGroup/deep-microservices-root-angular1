abbeaa25042832a895a8174bec6339b2c8ae6793
Vitali Cernomschi <vcernomschi@mitocgroup.com>
2016-06-27 17:49:29 +0300

    Regenerate backend tests & coverage optimization (#82)
    * Regenerate tests & coverage calculation optimization
    
    * Add commas
    
    * Fix bitwise warning

--- src/deep-root-angular/tests/backend/test/scheduler/rule/handler.spec.js
+++ src/deep-root-angular/tests/backend/test/scheduler/rule/handler.spec.js
@@ -1,13 +1,43 @@
-// THIS TEST WAS GENERATED AUTOMATICALLY ON Thu May 05 2016 16:19:05 GMT+0300 (EEST)
+// THIS TEST WAS GENERATED AUTOMATICALLY ON Mon Jun 27 2016 14:00:12 GMT+0300 (EEST)
 
 'use strict';
 
 import chai from 'chai';
-import Handler from '../../../node_modules/scheduler/rule/Handler';
+import Handler from '../../../../../backend/src/scheduler/rule/Handler';
+import Kernel from '../../../node_modules/deep-framework/node_modules/deep-kernel';
+import KernelFactory from '../../common/KernelFactory';
 
 // @todo: Add more advanced tests
 suite('Handlers', () => {
+  let handler, kernelInstance;
+
   test('Class Handler exists in deep-system-scheduler-rule module', () => {
     chai.expect(Handler).to.be.an('function');
   });
+
+  test('Load Kernel by using Kernel.load()', (done) => {
+    let callback = (backendKernel) => {
+      kernelInstance = backendKernel;
+
+      chai.assert.instanceOf(
+        backendKernel, Kernel, 'backendKernel is an instance of Kernel'
+      );
+
+      // complete the async
+      done();
+    };
+
+    KernelFactory.create(callback);
+  });
+
+  test('Check Handler constructor', () => {
+    handler = new Handler(kernelInstance);
+
+    chai.expect(handler).to.be.an.instanceof(Handler);
+  });
+
+  test('Check handle method exists', () => {
+    chai.expect(handler.handle).to.be.an('function');
+  });
+
 });
