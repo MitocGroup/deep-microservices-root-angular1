5575d89f75a8502405b2df3b74521b392704b08b
ddimitrioglo <ddimitrioglo@pycoding.biz>
2017-09-12 12:00:55 +0300

    #ROOT-NG1 fix coverage badge

--- src/deep-root-angular1/test/unit/hook.init.spec.js
+++ src/deep-root-angular1/test/unit/hook.init.spec.js
@@ -6,8 +6,6 @@ const hookInit = require('../../hook.init');
 describe('Check hook.init', () => {
 
   it('Test hook.init to be executable', done => {
-    let callback = sinon.spy();
-
     hookInit(function() {
       done();
     });
